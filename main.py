f63f70b318990705a4a7c8832a609d72abd2e46e
Harvey Rawlings <100100243+harraw42@users.noreply.github.com>
2023-03-13 23:25:59 +0000

    Added commands command and comment codes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# Import necessary modules
 import os
 import discord
 import random
 from discord.ext import commands
 
+# Get bot token from environment variable
 TOKEN = os.environ['Token']
 
 intents = discord.Intents.default()
@@ -10,36 +12,76 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="*", intents=intents)
 
+@bot.command(name="commands")
+async def commands(ctx):
+  await ctx.channel.send"""
+  ----- C O M M A N D S -----
+  (Prefix a command with "*")
+
+  *d4 - rolls a d4
+
+  *d6 - rolls a d6
+
+  *d8 - rolls a d8
+
+  *d10 - rolls a d10
+
+  *d12 - rolls a d12
+
+  *d20 - rolls a d20
+
+  *d100 - rolls a d100
+
+  *roll {enter number of dice} d{enter type of dice} - rolls the requested number of the 
+  requested dice
+
+  *tavern - plays tavern music
+
+  *battle - plays battle music
+
+  *travel - plays travel music
+
+  *ambience - plays ambient music
+  """
+
+#Define a command for rolling each singular die, with a send command to send "Rolling a {die}" then the result
+#d4
 @bot.command(name="d4")
 async def rolld4(ctx):
   d4 = random.randint(1, 4)
   await ctx.channel.send(f"Rolling a d4: {d4}")
 
+#d6
 @bot.command(name="d6")
 async def rolld6(ctx):
   d6 = random.randint(1, 6)
   await ctx.channel.send(f"Rolling a d6: {d6}")
 
+#d8
 @bot.command(name="d8")
 async def rolld8(ctx):
   d8 = random.randint(1, 8)
   await ctx.channel.send(f"Rolling a d8: {d8}")
 
+#d10
 @bot.command(name="d10")
 async def rolld10(ctx):
   d10 = random.randint(1, 10)
   await ctx.channel.send(f"Rolling a d10: {d10}")
 
+#d12
 @bot.command(name="d12")
 async def rolld12(ctx):
   d12 = random.randint(1, 12)
   await ctx.channel.send(f"Rolling a d12: {d12}")
 
+#d20
 @bot.command(name="d20")
 async def rolld20(ctx):
   d20 = random.randint(1, 20)
   await ctx.channel.send(f"Rolling a d20: {d20}")
 
+#d100
 @bot.command(name="d100")
 async def rolld100(ctx):
   d100 = random.randint(1, 100)
